d6b025a6cceea48d66eb07e0eb3753ae904a9ee3
jS <92333071+1jaycesagvold1@users.noreply.github.com>
2022-12-07 16:24:44 +0000

    Added function and class

---
+++ main.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+class AccountDetails {
+
+  let username: String
+  
+  let fallowers: Int
+
+  let url: URL
+
+  let user: String
+}
+
+
+struct CallAccount {
+
+    func UserCallFunction -> String {
+
+      let JohnAdams: user = "ActiveUser"
+
+      var JohnAdamsUsernam: username = "JohnAdams0151"
+
+      var JohnAdamsFallowers: fallowers
+
+
+      print(JohnAdamsFallowers)
+    }
+}
